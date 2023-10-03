564a05c382e5aa24873a3024d8c1c142f85d5989
JohnyDev <john.bedeir@gmail.com>
2023-10-04 00:55:25 +0200

    Fix #5 automate-destroy-script

---
+++ destroy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+namespace="go-survey"
+region="eu-central-1"
+cluster_name="cluster-1"
+repo_name="goapp-survey"
+
+# delete Docker-img from ECR
+echo "--------------------Deleting ECR-IMG--------------------"
+aws ecr batch-delete-image --repository-name $repo_name --image-ids imageTag=latest
+
+# delete deployment
+echo "--------------------Deleting Deployment--------------------"
+kubectl delete -n $namespace -f k8s/
+
+# delete namespace
+echo "--------------------Deleting Namespace--------------------"
+kubectl delete ns $namespace
+
+# delete AWS resources
+echo "--------------------Deleting AWS Resources--------------------"
+cd terraform && \
+terraform destroy -auto-approve
